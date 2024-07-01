6b1c731c27eb060c5aca1b6726045ea443dfb848
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-07-01 08:33:12 +0100

    more stuff

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,37 @@
 fun main(args: Array<String>) {
-    println("Hello world")
+    println("args: $args")
+    // Immutable variable
+    val name: String = "John"
+
+    // Mutable variable
+    var age: Int = 25
+
+    // nullable variable
+    var job: String? = null
+
+    println("Hello world", name, age)
+
+    // for loop
+    for (i in 1..5) {
+        println("current iteration: $i")
+    }
+
+    // while loop
+    var count = 0
+    while (count < 5) {
+        println("current (while) iteration: $count")
+        count++
+    }
+
+    // do while loop
+    do {
+        println("current (do-while) iteration: $count")
+        count++
+    } while (count < 0)
+
+    println(add(1, 2), " : add(1, 2)")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
 }
